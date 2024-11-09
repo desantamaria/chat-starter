0e0db4ff863c1a95a4fb4e0d56bdf7b5c6dea529
desantamaria <santamaria.daniel316@gmail.com>
2024-11-09 14:10:55 -0800

    Add Channels

--- src/app/(dashboard)/servers/[serverId]/channels/[channelId]/server-sidebar.tsx
+++ src/app/(dashboard)/servers/[serverId]/channels/[channelId]/server-sidebar.tsx
@@ -8,25 +8,49 @@ import {
   SidebarGroupLabel,
   SidebarHeader,
   SidebarMenu,
+  SidebarMenuAction,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { api } from "../../../../../../../convex/_generated/api";
 import { Id } from "../../../../../../../convex/_generated/dataModel";
+import { CreateChannel } from "../../create-channel";
+import { TrashIcon } from "lucide-react";
+import { toast } from "sonner";
 
 export function ServerSidebar({ id }: { id: Id<"servers"> }) {
   const pathname = usePathname();
   const server = useQuery(api.functions.server.get, { id });
-  const channels = useQuery(api.functions.server.channels, { id });
+  const channels = useQuery(api.functions.channel.list, { id });
+  const removeChannel = useMutation(api.functions.channel.remove);
+  const router = useRouter();
+
+  const handleChannelDelete = async (id: Id<"channels">) => {
+    try {
+      if (server) {
+        router.push(
+          `/servers/${server._id}/channels/${server?.defaultChannelId}`
+        );
+      }
+      await removeChannel({ id });
+      toast.success("Channel deleted");
+    } catch (error) {
+      toast.error("Failed to delete channel", {
+        description:
+          error instanceof Error ? error.message : "An unknown error occurred",
+      });
+    }
+  };
   return (
     <Sidebar className="left-12">
       <SidebarHeader>{server?.name}</SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Channels</SidebarGroupLabel>
+          <CreateChannel serverId={id} />
           <SidebarGroupContent>
             <SidebarMenu>
               {channels?.map((channel) => (
@@ -41,6 +65,11 @@ export function ServerSidebar({ id }: { id: Id<"servers"> }) {
                       {channel.name}
                     </Link>
                   </SidebarMenuButton>
+                  <SidebarMenuAction
+                    onClick={() => handleChannelDelete(channel._id)}
+                  >
+                    <TrashIcon />
+                  </SidebarMenuAction>
                 </SidebarMenuItem>
               ))}
             </SidebarMenu>
